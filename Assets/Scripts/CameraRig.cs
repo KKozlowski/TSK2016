1f70c49c04fba63ba900d60fdf70341d91697999
kkozlowski <septuater@gmail.com>
2016-11-15 00:11:38 +0100

    Added CameraRig for looking around the scene.

---
+++ Assets/Scripts/CameraRig.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraRig : MonoBehaviour
+{
+    private Vector2 mousePositionStart;
+    private Vector3 startEuler;
+
+	// Update is called once per frame
+	void Update () {
+	    if (Input.GetMouseButtonDown(1))
+	    {
+	        mousePositionStart = Input.mousePosition;
+	        startEuler = transform.rotation.eulerAngles;
+	    } else if (Input.GetMouseButton(1))
+	    {
+	        Vector2 mousePositionNow = Input.mousePosition;
+	        Vector3 newEuler = startEuler + 0.5f*new Vector3(-mousePositionNow.y + mousePositionStart.y, mousePositionNow.x - mousePositionStart.x, 0);
+	        transform.rotation = Quaternion.Euler(newEuler);
+	    }
+	}
+}
